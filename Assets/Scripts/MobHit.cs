4b79f76f51e12fb881277f0298869b09d9c8ea0e
Yunbo Shim <ybuser@naver.com>
2023-02-23 20:39:52 +0900

    2/23 : Feature > Sword & Bow

---
+++ Assets/Scripts/MobHit.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobHit : MonoBehaviour
+{
+    [SerializeField]
+    public int hp = 100;
+
+    bool isHit = false;
+
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Weapon")){
+
+            if(!isHit){
+
+                hp -= 20;
+                
+            }
+
+            
+            
+            Debug.Log($"Hp : {hp}");
+
+            if(hp <= 0){
+                Destroy(gameObject);
+            }
+            
+            isHit = true;
+
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other) {
+        isHit = false;
+        
+    }
+
+    
+
+
+    
+}
